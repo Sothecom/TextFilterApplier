48a39efcc6da9663eecb594d0e2364ef189414f0
sodeyir <sodeyir@gmail.com>
2022-07-09 21:01:37 +0100

    Refining tests

--- Tests/TextFilterApplierFilterProviderUnitTests/TextFilterHelperUnitTests.cs
+++ Tests/TextFilterApplierFilterProviderUnitTests/TextFilterHelperUnitTests.cs
@@ -13,28 +13,32 @@ namespace Tests.TextFilterApplierFilterProviderUnitTests
         [DataRow("The", "h")]
         public void GivenAnOddLengthStringGreaterThan2_WhenGetMiddle_ThenGet1MiddleLetter(string input, string expectedResult)
         {
-            Assert.AreEqual(input, expectedResult);
+            string actualResult = string.Empty;
+            Assert.AreEqual(actualResult, expectedResult);
         }
 
         [TestMethod]
         [DataRow("Forces", "rc")]
         public void GivenAnEvenLengthStringGreaterThan1_WhenGetMiddle_ThenGet2MiddleLetters(string input, string expectedResult)
         {
-            Assert.AreEqual(input, expectedResult);
+            string actualResult = string.Empty;
+            Assert.AreEqual(actualResult, expectedResult);
         }
 
         [TestMethod]
         [DataRow("b", "b")]
         public void GivenAnOddLengthStringLessThan2_WhenGetMiddleApplied_ThenGetSameLetter(string input, string expectedResult)
         {
-            Assert.AreEqual(input, expectedResult);
+            string actualResult = string.Empty;
+            Assert.AreEqual(actualResult, expectedResult);
         }
 
         [TestMethod]
         [DataRow("wd", "wd")]
         public void GivenAnEvenLengthStringLessThan3_WhenGetMiddleApplied_GetSame2MiddleLetters(string input, string expectedResult)
         {
-            Assert.AreEqual(input, expectedResult);
+            string actualResult = string.Empty;
+            Assert.AreEqual(actualResult, expectedResult);
         }
         #endregion
 
@@ -52,9 +56,11 @@ namespace Tests.TextFilterApplierFilterProviderUnitTests
 
         #region String contains string helper Tests
         [TestMethod]
-        [DataRow("Tony", "t", true)]
+        [DataRow("Tony", "t", false)]
+        [DataRow("tony", "t", true)]
         [DataRow("Ja", "t", false)]
-        [DataRow("Helper", "help", true)]
+        [DataRow("Helper", "Help", true)]
+        [DataRow("Case Sensitive", "se S", true)]
         public void GivenAString_WhenViolatesForbiddenStringCheckMade_ReturnsExpectedBooleanResult(string input, string forbiddenString, bool expectedResult)
         {
             bool actualResult = input.Contains(forbiddenString);
